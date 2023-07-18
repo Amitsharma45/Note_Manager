48ae6db63614feb020ce4b9f3807d3dec8969270
Amitsharma45 <sharma.amit20111@gmaail.com>
2023-07-18 16:32:32 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express= require('express');
+const app= express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const {router} = require('./Routers/router')
+require('dotenv').config()
+
+//middle ware
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('api/v1',router)
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is running at port 5000")
+})
+
