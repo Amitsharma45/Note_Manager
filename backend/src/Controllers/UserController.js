0f828194d13f8fdcdcd89888dd6166ea57df7922
Amitsharma45 <sharma.amit20111@gmaail.com>
2023-07-28 10:31:22 +0530

    C:/Program Files/Git/createnote route completd

--- backend/src/Controllers/UserController.js
+++ backend/src/Controllers/UserController.js
@@ -2,14 +2,12 @@ const repo= require('../Repository/UserRepo')
 
 const createNote = (req,res) =>{
     repo.createNote(req).then((data)=>{
-        console.log(data)
         res.status(200).send({
             status: 200,
             message: data
         });
         
     }).catch((err)=>{
-        console.log(err)
         res.status(404).send('Something went wrong')
     })
 }
--- backend/src/Repository/UserRepo.js
+++ backend/src/Repository/UserRepo.js
@@ -2,33 +2,37 @@ const NoteModule = require('../Modules/NoteModule');
 const { v4: uuidv4 } = require('uuid');
 const createNote = (req) => {
     return new Promise((resolve, reject) => {
-        NoteModule.find({_id : req.body._id}).then((data)=>{
-            console.log(!data)
-            if(data){
-                resolve('have data');
-            }else{
-                reject('emopty')
+        NoteModule.find({ _id: req.body._id }).then((data) => {
+            if (data.length!==0) {
+                NoteModule.findOneAndUpdate({ _id: req.body._id }, { $push: { notes: { _id: uuidv4(), tittle: req.body.tittle, notebody: req.body.notebody } } }).then((data) => {
+                    if (data) {
+                        resolve('Data Push Success');
+                    } else {
+                        reject('Something went wrong')
+                    }
+                }).catch((err) => {
+                    reject(err)
+                })
+            } else {
+                let newNote = new NoteModule({
+                    _id: req.body._id,
+                    notes: [{
+                        _id: uuidv4() ,
+                        tittle: req.body.tittle,
+                        notebody: req.body.notebody
+                    }]
+                })
+                newNote.save().
+                    then(() => {
+                        resolve('New array created')
+                    })
+                    .catch((err) => {
+                        reject(err)
+                    })
             }
-        }).catch((err)=>{
+        }).catch((err) => {
             reject(err)
         })
-
-
-    //     let newNote = new NoteModule({
-    //         _id:  uuidv4() ,
-    //         notes: [{
-    //             _id:  uuidv4() ,
-    //             tittle: req.body.tittle,
-    //             notebody: req.body.notebody
-    //         }]
-    //     })
-    //     newNote.save().
-    //         then(() => {
-    //             resolve('Working')
-    //         })
-    //         .catch((err) => {
-    //             reject(err)
-    //         })
     })
 
 }
