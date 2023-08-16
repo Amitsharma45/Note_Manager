57bc558c22d10b508ed9cea404147fee0a5a892b
Amitsharma45 <sharma.amit20111@gmaail.com>
2023-08-16 15:58:37 +0530

    readme file update

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -75,7 +75,7 @@ function Card(props) {
     }
 
     return (
-        <div className='hover:scale-105 transition-all ease-in-out duration-100'>
+        <div className=''>
             <div className='md:w-[350px] w-[300px] overflow-hidden h-min-[250px]  border-black bg-inputlight  dark:bg-black dark:bg-opacity-10 dark:text-white text-slate-800  rounded-xl my-5 relative'>
                 <div className='border-b-[2px] border-b-gray-700 dark:border-b-slate-900 px-5 cursor-pointer pt-4 pb-2 text-xl flex justify-between'>
                     <div> {item.tittle}</div>
